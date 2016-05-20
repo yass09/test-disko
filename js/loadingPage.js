087213647a61fd0a800028e96766cbd9dc6be6ed
yass09 <y.chibane@me.com>
2016-05-21 00:56:40 +0200

    added instructions

--- js/loadingPage.js
+++ js/loadingPage.js
@@ -1,12 +1,16 @@
 // On page load, enter Konami Code to
 $(document).ready(function(){
+  // Variable pour appeler le fichier audio
   var audio = document.getElementById('SF2'),
+  // Fonction pour lancer l'audio
       playSound = function () {
         if (!$("audio").hasClass("played")){
           audio.play();
           $("audio").addClass("played");
         }
-      };
+      },
+  // Declaration du Konami code
+  // Options contient le code a effectuer et la fonction déclenchée par le code
       options = {
         pattern: [40,39].join(""),
         onPatternMatch: function (e,data){
@@ -14,18 +18,19 @@ $(document).ready(function(){
           playSound ();
           console.log("keys pressed: " + event.which);
         }
+      },
+  // fonctions pour faire apparaître les instructions
+      addText = function () {
+        $(".loadingPage_text").append("<p>Sur smartphone, un tap suffira.</p>");
+      },
+      addText2 = function (a) {
+        $(".loadingPage_text").append("<p>OK, bon un click suffira sur desktop.</p>")
       };
-
+  // SetTimeout faisant apparaitre les instructions
+    setTimeout(addText,2000);
+    setTimeout(addText2,5000);
     $(document).konami(options).click(function () {
-        // $(".loadingPage_text").text("Pour entrer tapper l'ecran");
         $(".loadingPage").fadeOut("slow");
         playSound ();
     });
-
-
-    // keydown(function (event) {
-    //     $(".loadingPage").fadeOut("slow");
-    //     playSound ();
-    //   console.log("keys pressed" + event.which);
-    // });
 });
